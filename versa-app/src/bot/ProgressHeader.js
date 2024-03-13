1aa89878069096cf4dbd9dd0bca95045dcbdd4ec
rathnapriya-radcube <priyag@radcube.com>
2024-03-13 16:58:50 -0400

    Adding custom message - welcome + Changing Bot Styles

---
+++ versa-app/src/bot/ProgressHeader.js
@@ -0,0 +1,43 @@
+import React from "react";
+import smileyPic from "../assets/images/smiley-pic.png"; // Import the image
+import versaLogo from "../assets/images/versa-logo.png"; // Import the image
+import "../styles/styles.css";
+const ProgressHeader = () => {
+  return (
+    <div className="outer-wrapper">
+      <div className="outer-container">
+        <div className="image-container">
+          <img src={versaLogo}></img>
+        </div>
+        <div className="chat-body-container">
+          <div className="profile-image-container">
+            <img src={smileyPic}></img>
+          </div>
+          <div className="welcome-card">
+            <h3>Let's get you started</h3>
+            <p>
+              I will help you check your "Oral, Sexual and Mental health
+              interconnections, and it's "chance scores" for guided testing and
+              care connections.
+              <br></br>
+              Spend only 6-10 minutes with me to get your chance score.
+              Although, some questions may require more time and attention.
+            </p>
+            <button
+              key="get-started"
+              id="get-started"
+              className="get-started-class"
+            >
+              Get your Chance Score now
+            </button>
+            <button key="know-more" id="know-more" className="know-more-class">
+              Know More
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressHeader;
--- versa-app/src/bot/config.js
+++ versa-app/src/bot/config.js
@@ -1,13 +1,17 @@
 // in config.js
-import { createChatBotMessage } from "react-chatbot-kit";
+// import { createChatBotMessage } from "react-chatbot-kit";
 import logo from "../assets/images/profile.jpg"; // Import the image
 import GeneralOptions from "./widgets/GeneralOptions";
 import YNQuestions from "./widgets/YNQuestions";
+import CustomMessage from "./CustomMessage";
 
-const botName = "Versa OSM Buddy";
+import { createChatBotMessage, createCustomMessage } from "react-chatbot-kit";
+const botName = "OSM Buddy";
 
 const config = {
   initialMessages: [
+    createCustomMessage("Test", "custom"),
+
     createChatBotMessage(
       `Hey there, I'm Versa, your OSM buddy. Your go-to shop for anything related to Oral, Sexual and Mental health awareness and care connections.  \n\n
     `,
@@ -109,6 +113,9 @@ const config = {
     //  userAvatar: (props) => <MyCustomAvatar {...props} />,
     //  // Replaces the default user chat message
     //  userChatMessage: (props) => <MyCustomUserChatMessage {...props} />
+  },
+  customMessages: {
+    custom: (props) => <CustomMessage {...props} />,
   },
 };
 
