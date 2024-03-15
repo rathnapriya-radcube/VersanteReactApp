0adfc6180eeda41971bd8c9445b4a01516585186
rathnapriya-radcube <priyag@radcube.com>
2024-03-15 16:47:41 -0400

    Know More ToolTip Included + Fixes

--- versa-app/src/bot/CustomMessage.js
+++ versa-app/src/bot/CustomMessage.js
@@ -4,6 +4,74 @@ import versaLogo from "../assets/images/versa-logo.png"; // Import the image
 import "../styles/styles.css";
 const CustomMessage = (props) => {
   console.log("custom message", props);
+
+  let showKnowMoreToolTip = false;
+  const displayToolTip = () => {
+    console.log(" displayToolTip showKnowMoreToolTip", showKnowMoreToolTip);
+    if (showKnowMoreToolTip) {
+      const toolTipButton = document.getElementsByClassName(
+        "tooltip-button-welcome-page"
+      )[0];
+      toolTipButton.disabled = true;
+      const containerA = document.createElement("div");
+      containerA.id = "containerA"; // Assign ID to container A
+      const heading = document.createElement("h2");
+      heading.id = "know-more-heading"; // Assign ID to heading
+      const paragraph = document.createElement("div");
+      paragraph.id = "paragraph"; // Assign ID to paragraph
+      const paragraphElement = document.createElement("p");
+      paragraphElement.id = "know-more-paragraph-element";
+      paragraphElement.innerHTML =
+        'OSM chance score" is a simple evaluation of Oral, Sexual and Mental health and that unlocks the chance of interconnected risks. Assess your Chance Score anonymously and delete your score once you receive it, anytime, anywhere. <br /><br />Also avail assistance on timely screenings, free tests, and peer education sessions. Don\'t miss out on your "chance” to be on top of your OSM game! ';
+      const button = document.createElement("button");
+      button.id = "know-more-button"; // Assign ID to button
+      button.addEventListener("click", () => {
+        console.log("Button click", showKnowMoreToolTip);
+        handleKnowMoreClick();
+      });
+      // Set content
+      heading.textContent = "What is OSM Chance Score?";
+      button.textContent = "Dismiss";
+
+      // Append elements to container A
+      containerA.appendChild(heading);
+      paragraph.appendChild(paragraphElement);
+      containerA.appendChild(paragraph);
+      containerA.appendChild(button);
+
+      // Get parent of container B
+      const parentOfB = document.getElementsByClassName(
+        "react-chatbot-kit-chat-input-container"
+      )[0].parentNode;
+
+      // Attach container A after container B
+      parentOfB.insertBefore(
+        containerA,
+        document.getElementsByClassName(
+          "react-chatbot-kit-chat-input-container"
+        )[0].nextSibling
+      );
+    } else {
+      const toolTipButton = document.getElementsByClassName(
+        "tooltip-button-welcome-page"
+      )[0];
+      toolTipButton.disabled = false;
+      const containerA = document.getElementById("containerA");
+
+      if (containerA) {
+        containerA.parentNode.removeChild(containerA);
+      }
+    }
+  };
+  const handleKnowMoreClick = () => {
+    console.log("handleKnowMoreClick ", showKnowMoreToolTip);
+    showKnowMoreToolTip = !showKnowMoreToolTip;
+    setTimeout(displayToolTip, 0);
+
+    // setShowToolTip((prevState) => !prevState);
+
+    // Call displayToolTip after state has been updated
+  };
   const handleGetStarted = () => {
     console.log("Called handleGetStarted");
     let outerWrapperContainer =
@@ -17,19 +85,19 @@ const CustomMessage = (props) => {
     outerWrapperContainer.insertAdjacentElement("afterend", initialPaddingDiv);
     props.actions.handleOptIn();
   };
-  const handleKnowMoreClick = () => {
-    console.log("Called handleKnowMoreClick");
-    let outerWrapperContainer =
-      document.getElementsByClassName("outer-wrapper")[0];
-    console.log("Before", outerWrapperContainer);
-    outerWrapperContainer.style.display = "none";
-    console.log("After", outerWrapperContainer);
-    let initialPaddingDiv = document.createElement("div");
-    initialPaddingDiv.style.padding = "20px";
-    initialPaddingDiv.style.backgroundColor = "#cdc7e3fd";
-    outerWrapperContainer.insertAdjacentElement("afterend", initialPaddingDiv);
-    props.actions.handleKnowMore();
-  };
+  // const handleKnowMoreClick = () => {
+  //   console.log("Called handleKnowMoreClick");
+  //   let outerWrapperContainer =
+  //     document.getElementsByClassName("outer-wrapper")[0];
+  //   console.log("Before", outerWrapperContainer);
+  //   outerWrapperContainer.style.display = "none";
+  //   console.log("After", outerWrapperContainer);
+  //   let initialPaddingDiv = document.createElement("div");
+  //   initialPaddingDiv.style.padding = "20px";
+  //   initialPaddingDiv.style.backgroundColor = "#cdc7e3fd";
+  //   outerWrapperContainer.insertAdjacentElement("afterend", initialPaddingDiv);
+  //   // props.actions.handleKnowMore();
+  // };
   return (
     <div className="outer-wrapper">
       <div className="outer-container">
